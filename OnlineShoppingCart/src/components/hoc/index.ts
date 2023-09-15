b799f09e3170b2380e218af2705e298175342f75
et3 labs <et3labs@et3s-MacBook-Pro.local>
2023-09-15 16:10:51 +0300

    hoc update

--- OnlineShoppingCart/src/components/hoc/index.ts
+++ OnlineShoppingCart/src/components/hoc/index.ts
@@ -1,25 +1,39 @@
 import {ComponentType, ReactNode} from 'react';
-import {TEt3Theme} from '../et3-theme/';
 import {observer} from 'mobx-react';
 // import compose from '../Recompose';
+import {COLORS} from '../../constants';
+import compose from 'lodash/fp/compose';
 
 export type WithLiteObserver = <T extends {}>(
   component: ComponentType<T>,
   defaultProps?: T,
 ) => ComponentType<T>;
 
-// export const withLiteObserver = compose(LiteObserver) as WithLiteObserver;
+// export const withLiteObserver = (component => {
+//   return compose(observer)(component);
+// }) as WithLiteObserver;
+
+export const withLiteObserver = compose(observer) as WithLiteObserver;
 
 // export const withLiteObserver = (props: ({product}: TProduct) => JSX.Element) =>
 //   observer(props) as unknown as WithLiteObserver;
 
-export const withLiteObserver = props => observer(props) as WithLiteObserver;
+// export const withLiteObserver = (ComponentTest) => {
+
+//   return <ComponentTest  COLORS=COLORS/>
+// };
+
+// export const withLiteObserver = props => observer(props) as WithLiteObserver;
 
 type WithThemeAndLiteObserver = <T extends {}>(
   component: ComponentType<T & {theme: TEt3Theme}>,
   defaultProps?: T,
 ) => ComponentType<T & {theme?: TEt3Theme}>;
 
+// export const withLiteObserverAndTheme = (component => {
+//   return compose(withLiteObserver, COLORS)(component);
+// }) as WithThemeAndLiteObserver;
+
 export const withLiteObserverAndTheme = (component => {
-  return compose(observer)(component);
+  return compose(observer, COLORS)(component);
 }) as WithThemeAndLiteObserver;
--- OnlineShoppingCart/src/components/profile/Profile.tsx
+++ OnlineShoppingCart/src/components/profile/Profile.tsx
@@ -5,14 +5,15 @@ import {Tnavigation} from '../../types';
 import UsernameInput from './UsernameInput';
 import AddressInput from './AddressInput';
 import Submit from './Submit';
-import {withLiteObserver} from '../hoc';
+import {withLiteObserver, withLiteObserverAndTheme} from '../hoc';
 
-const Profile = ({navigation}: Tnavigation) => {
+const Profile = (props, {navigation}: Tnavigation) => {
   const [name, setName] = useState<string>(userStore?.user.name ?? '');
   const [address, setAddress] = useState<string>(userStore?.user.address ?? '');
 
   return (
     <View style={{marginHorizontal: 10}}>
+      {console.log(props.COLORS)}
       <UsernameInput name={name} setName={setName} />
       <AddressInput address={address} setAddress={setAddress} />
       <Submit navigation={navigation} name={name} address={address} />
