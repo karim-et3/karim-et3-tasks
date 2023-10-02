221cb81080c5b403738a8a72958e4fa664de02a5
et3 labs <et3labs@et3s-MacBook-Pro.local>
2023-10-02 14:28:56 +0300

    added flatlist

--- NewsReaderAppwithThemes/src/components/Home/ListNews.tsx
+++ NewsReaderAppwithThemes/src/components/Home/ListNews.tsx
@@ -1,17 +1,15 @@
-import {ScrollView} from 'react-native';
+import {FlatList} from 'react-native';
 import React from 'react';
-import {Tnews} from '../../types';
 import DisplaySingleNews from './DisplaySingleNews';
 import {observer} from 'mobx-react-lite';
 import newsStore from '../../mobx/News';
 
 const ListNews = () => {
   return (
-    <ScrollView>
-      {newsStore.news.map((news: Tnews) => (
-        <DisplaySingleNews news={news} key={news.publishedAt} />
-      ))}
-    </ScrollView>
+    <FlatList
+      data={newsStore.getNews}
+      renderItem={({item}) => <DisplaySingleNews news={item} />}
+    />
   );
 };
 
